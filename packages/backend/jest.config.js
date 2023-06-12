88f0e18f7d8e99277d3c42f9f007029e1d3698ac
npham49 <brian.d.pham@gov.bc.ca>
2023-06-11 17:35:37 -0700

    fix: tests update database

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -98,7 +98,7 @@ exports.default = {
   // The paths to modules that run some code to configure or set up the testing environment before each test
   // setupFiles: [],
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["<rootDir>/src/db/singleton.ts"],
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
